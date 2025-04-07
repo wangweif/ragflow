b29ac6907085ddc767b70dc5abc2e19543f6c91c
HuoHaimeng <13483147669@163.com>
2025-04-07 09:41:55 +0800

    租户信息接口调整

--- api/db/services/user_service.py
+++ api/db/services/user_service.py
@@ -193,7 +193,7 @@ class TenantService(CommonService):
             cls.model.parser_ids,
             UserTenant.role]
         return list(cls.model.select(*fields)
-                    .join(UserTenant, on=((cls.model.id == UserTenant.tenant_id) & (UserTenant.user_id == user_id) & (UserTenant.status == StatusEnum.VALID.value) & (UserTenant.role == UserTenantRole.OWNER)))
+                    .join(UserTenant, on=((cls.model.id == UserTenant.tenant_id) & (UserTenant.user_id == user_id) & (UserTenant.status == StatusEnum.VALID.value)))
                     .where(cls.model.status == StatusEnum.VALID.value).dicts())
 
     @classmethod
