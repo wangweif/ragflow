39938bd59476cd08d2f5325037ec6ec4d99ca89c
ChengZhikai <buct255@gmail.com>
2025-07-28 09:14:00 +0800

    feat(wrapper): 防止非创建者修改知识库配置

--- web/src/wrappers/knowledgeConfiguration.tsx
+++ web/src/wrappers/knowledgeConfiguration.tsx
@@ -1,9 +1,11 @@
+import { useFetchKnowledgeBaseConfiguration } from '@/hooks/knowledge-hooks';
 import { useFetchUserInfo } from '@/hooks/user-setting-hooks';
 import { Navigate, Outlet } from 'umi';
 
 export default () => {
   const { data: userInfo } = useFetchUserInfo();
-  if (userInfo?.id !== userInfo.tenant_id) {
+  const { data: knowledgeDetails } = useFetchKnowledgeBaseConfiguration();
+  if (userInfo?.id !== knowledgeDetails.created_by) {
     return <Navigate to="/knowledge" />;
   } else {
     return <Outlet />;
