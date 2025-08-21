383e93594e5667e45f2a92b38a9fff957bb29703
ChengZhikai <buct255@gmail.com>
2025-08-21 10:43:04 +0800

    fix: 修复redis失效时无限循环错误

--- api/ragflow_server.py
+++ api/ragflow_server.py
@@ -65,9 +65,10 @@ def update_progress():
             if redis_lock.acquire():
                 DocumentService.update_progress()
                 redis_lock.release()
-            stop_event.wait(6)
         except Exception:
             logging.exception("update_progress exception")
+        finally:
+            stop_event.wait(6)
 
 def signal_handler(sig, frame):
     logging.info("Received interrupt signal, shutting down...")
